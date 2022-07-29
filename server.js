713cbb284cd16a8dbdd713b11e73560ee9383bda
Junghoon Yoon <jyoon3@2u.com>
2022-07-29 13:07:58 -0400

    Tables do be making

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const sequelize = require("./config/connection");
 
+const model = require("./models");
+
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => {
     console.log("Listening on PORT # http://localhost:3001");
   });
