61e4da5ab397a02a8b3c307403d5ded237c05209
Junghoon Yoon <jyoon3@2u.com>
2022-07-29 13:40:14 -0400

    Pushing just home to display page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,12 +3,12 @@ const { User } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
-    const UserInfo = await User.findByPk(1);
+    // const UserInfo = await User.findByPk(1);
 
-    // TO convert my Sequelize object into a useable object
-    const serializedUserData = UserInfo.get({ plain: true });
-    console.log(serializedUserData);
-    res.render("home", serializedUserData);
+    // // TO convert my Sequelize object into a useable object
+    // const serializedUserData = UserInfo.get({ plain: true });
+    // console.log(serializedUserData);
+    res.render("home");
   } catch (err) {
     res.status(500).json(err);
   }
