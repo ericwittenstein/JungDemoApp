d6238a6e50000e084eef115eee9a329741b29992
Junghoon Yoon <jyoon3@2u.com>
2022-07-29 13:36:16 -0400

    user data displays to the page

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const path = require("path");
 const express = require("express");
+const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection");
 const routes = require("./controllers");
-
-// const model = require("./models");
-
 const app = express();
+
+const hbs = exphbs.create({});
+// const model = require("./models");
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
